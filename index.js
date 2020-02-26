7ee9e436a8c5a16a66179ad8bab7fc1668bb737c
Cemil Yesil <cemilyesil@hotmail.com.tr>
2020-02-26 17:50:57 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,46 @@
+import cryptoJs from 'crypto-js';
+
+const md5x2 = (value) => {
+    return md5(md5(value));
+};
+
+const md5 = (value) => {
+    return md5Utf8(value);
+};
+
+const md5Utf8 = (value) => {
+    return cryptoJs.MD5(cryptoJs.enc.Utf8.parse(value)).toString();
+};
+
+const decrypt = (value, publicKey, privateKey) => {
+    try {
+        return cryptoJs.enc.Utf8.stringify(
+            cryptoJs.AES.decrypt(value, md5Utf8(publicKey), {
+                iv: md5Utf8(privateKey),
+                mode: cryptoJs.mode.CBC,
+                padding: cryptoJs.pad.Pkcs7
+            })
+        )
+    } catch (error) {
+        return null;
+    }
+};
+
+const encrypt = (value, publicKey, privateKey) => {
+    return cryptoJs.AES.encrypt(
+        cryptoJs.enc.Utf8.parse(value),
+        md5Utf8(publicKey),
+        {
+            iv: md5Utf8(privateKey),
+            mode: cryptoJs.mode.CBC,
+            padding: cryptoJs.pad.Pkcs7
+        }
+    ).toString();
+};
+
+export default {
+    md5x2,
+    md5,
+    decrypt,
+    encrypt,
+};
